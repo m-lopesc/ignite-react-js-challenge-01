e37631dd1561852bbc2e3d1eb283d83029407337
Mateus Castro <mlopesc3@icloud.com>
2021-08-27 00:07:36 -0300

    removendo uma task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -36,6 +36,8 @@ export function TaskList() {
 
 	function handleRemoveTask(id: number) {
 		// Remova uma task da listagem pelo ID
+
+		setTasks(tasks.filter(task => task.id !== id));
 	}
 
 	return (
