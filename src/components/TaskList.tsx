0f0dc4c7c8ad479504069bb396e787c67e918603
Mateus Castro <mlopesc3@icloud.com>
2021-08-27 00:05:57 -0300

    criando uma task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,6 +15,19 @@ export function TaskList() {
 
 	function handleCreateNewTask() {
 		// Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+		if (!newTaskTitle) return;
+
+		setTasks([
+			...tasks,
+			{
+				id: Math.random(),
+				title: newTaskTitle,
+				isComplete: false,
+			},
+		]);
+
+		setNewTaskTitle('');
 	}
 
 	function handleToggleTaskCompletion(id: number) {
